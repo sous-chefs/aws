92bf68c950cc230d1d03dc322e4066704b207c1f
Andrew Crump <andrew@kotirisoftware.com>
2012-03-26 17:14:26 +0100

    FC016: LWRP does not declare a default action.

--- resources/elastic_lb.rb
+++ resources/elastic_lb.rb
@@ -3,3 +3,8 @@ actions :register, :deregister
 attribute :aws_access_key,        :kind_of => String
 attribute :aws_secret_access_key, :kind_of => String
 attribute :name,                  :kind_of => String
+
+def initialize(*args)
+  super
+  @action = :register
+end
