c606e22be3ae63a717902baa6834051b621d4e16
Evan Gilman <evan@pagerduty.com>
2013-08-18 11:21:25 -0700

    [COOK-3475] Fix ebs_volume detach idempotency

--- providers/ebs_volume.rb
+++ providers/ebs_volume.rb
@@ -72,7 +72,6 @@ end
 
 action :detach do
   vol = determine_volume
-  return if vol[:aws_instance_id] != instance_id
   converge_by("detach volume with id: #{vol[:aws_id]}") do
     detach_volume(vol[:aws_id], new_resource.timeout)
   end
@@ -232,8 +231,9 @@ end
 
 # Detaches the volume and blocks until done (or times out)
 def detach_volume(volume_id, timeout)
-  Chef::Log.debug("Detaching #{volume_id}")
   vol = volume_by_id(volume_id)
+  return if vol[:aws_instance_id] != instance_id
+  Chef::Log.debug("Detaching #{volume_id}")
   orig_instance_id = vol[:aws_instance_id]
   ec2.detach_volume(volume_id)
 
