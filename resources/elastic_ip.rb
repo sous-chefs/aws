96ec02e39bcf61170c3171daf2fe9e23e7efd30a
Jean Mertz <jean@mertz.fm>
2014-02-25 13:15:20 -0500

    [COOK-4008] - Allow elastic_ip lwrp ip as name attribute
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- resources/elastic_ip.rb
+++ resources/elastic_ip.rb
@@ -6,7 +6,7 @@ state_attrs :aws_access_key,
 
 attribute :aws_access_key,        :kind_of => String
 attribute :aws_secret_access_key, :kind_of => String
-attribute :ip,                    :kind_of => String
+attribute :ip,                    :kind_of => String, :name_attribute => true
 attribute :timeout,               :default => 3*60 # 3 mins, nil or 0 for no timeout
 
 def initialize(*args)
