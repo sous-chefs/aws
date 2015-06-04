f89e64299bcf63acdd3b2c2fa79528e01d6abae2
Sean OMeara <sean@chef.io>
2015-06-04 13:57:24 -0400

    updating CHANGELOG and bumping metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'LWRPs for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.7.0'
+version '2.7.1'
 recipe 'aws', 'Installs the right_aws gem during compile time'
 source_url       "https://github.com/opscode-cookbooks/aws"
 issues_url       "https://github.com/opscode-cookbooks/aws/issues"
