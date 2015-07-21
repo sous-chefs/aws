d36629a28242f7efa8494a5fe23a04087829fb95
Matt Jones <matt.jones@needle.com>
2015-07-21 09:57:48 -0600

    Rebased Fork on Current AWS Book (2.7.2) and reversioned to our 14.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'LWRPs for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.7.2'
+version '14.2.0'
