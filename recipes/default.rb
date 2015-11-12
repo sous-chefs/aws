ba7a88d2f6dec540cb4a8bd01581b6a91d1f65b6
Tim Smith <tsmith84@gmail.com>
2015-11-12 11:36:57 -0800

    Add license headers to the recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 chef_gem 'aws-sdk' do
   version node['aws']['aws_sdk_version']
   compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
