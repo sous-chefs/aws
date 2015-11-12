d9a7c917afa48f9a5c62ec692348b5c24bd76c41
Tim Smith <tsmith84@gmail.com>
2015-11-11 22:29:29 -0800

    Opscode -> Chef Software

--- libraries/ec2.rb
+++ libraries/ec2.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: Copyright (c) 2009 Opscode, Inc.
+# Copyright:: Copyright (c) 2009-2015 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
