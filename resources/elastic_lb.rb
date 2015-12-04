1309cb17e23113d7f4bb6479c4acc7f562a2c050
Vyshegorodtsev, Marat | Marat | ITGD <marat.vyshegorodtsev@rakuten.com>
2015-12-04 12:16:53 +0900

    Removed unnecessary spacing in resources/elastic_lb.rb

--- resources/elastic_lb.rb
+++ resources/elastic_lb.rb
@@ -8,7 +8,7 @@ identity_attr :elb_name
 attribute :aws_access_key,        kind_of: String
 attribute :aws_secret_access_key, kind_of: String
 attribute :aws_session_token,     kind_of: String, default: nil
-attribute :elb_name,                  kind_of: String, name_attribute: true
+attribute :elb_name,              kind_of: String, name_attribute: true
 attribute :idle_timeout,          kind_of: Integer
 attribute :cross_zone,            kind_of: [TrueClass, FalseClass]
 attribute :enable_access_log,     kind_of: [TrueClass, FalseClass]
