96e73b7b0dea629bf9b7af5d2716ea565570da88
Tim Smith <tsmith84@gmail.com>
2016-01-19 14:43:39 -0800

    Release 2.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'LWRPs for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.7.2'
-recipe 'aws', 'Installs the right_aws gem during compile time'
+version '2.8.0'
+recipe 'aws', 'Installs the aws-sdk gem during compile time'
+recipe 'ec2_hints', 'Adds an EC2 hint file for Ohai cloud detection'
 source_url 'https://github.com/chef-cookbooks/aws' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/aws/issues' if respond_to?(:issues_url)
