9f51790dc530133f3dde77b70a6cab55fa9775c1
Tim Smith <tsmith84@gmail.com>
2016-01-20 16:38:30 -0800

    Rework the kitchen cloud config
    Hardcode the region / AZ since the AMIs are only in us-west-2
    Update for the latest kitchen-ec2 as most of the configs were deprecated
    Update to the latest platforms
    Only test on HVM systems
    Use T2.micros for everything

--- test/fixtures/cookbooks/aws_test/recipes/instance_monitoring.rb
+++ test/fixtures/cookbooks/aws_test/recipes/instance_monitoring.rb
@@ -1,18 +1,5 @@
 include_recipe 'aws'
-
-directory '/etc/chef/ohai/hints' do
-  recursive true
-  action :create
-end.run_action(:create)
-
-file '/etc/chef/ohai/hints/ec2.json' do
-  content {}
-  action :create
-end.run_action(:create)
-
-ohai 'reload' do
-  action :reload
-end.run_action(:reload)
+include_recipe 'aws::ec2_hints'
 
 aws_instance_monitoring 'enable detailed monitoring' do
   aws_access_key node['aws_test']['key_id']
