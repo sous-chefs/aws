dc8d7fb87536f369dab9eb56e994ddf624f4cd3b
Shaun Martin <shaun@samsite.ca>
2016-01-22 15:10:45 -0800

    [GH-169] s3 conditional requests
    - Adding basic logic for skipping :create when local & remote files match

--- test/fixtures/cookbooks/aws_test/recipes/s3_file.rb
+++ test/fixtures/cookbooks/aws_test/recipes/s3_file.rb
@@ -6,3 +6,11 @@ aws_s3_file '/tmp/an_file' do
   aws_access_key_id node['aws_test']['key_id']
   aws_secret_access_key node['aws_test']['access_key']
 end
+
+# do it again. this will produce an INFO log message and skip the actual action
+aws_s3_file '/tmp/an_file' do
+  bucket node['aws_test']['bucket']
+  remote_path node['aws_test']['s3key']
+  aws_access_key_id node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+end
