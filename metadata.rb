d6be61362cfb1544137a53245a5d32c09667ad7d
Tim Smith <tsmith84@gmail.com>
2016-01-26 14:23:45 -0800

    Add platforms to the metadata
    Otherwise we don't get the badges on supermarket

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,17 @@ name 'aws'
 maintainer 'Chef Software, Inc'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'LWRPs for managing AWS resources'
+description 'Custom resources for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.9.0'
+
 recipe 'aws', 'Installs the aws-sdk gem during compile time'
 recipe 'ec2_hints', 'Adds an EC2 hint file for Ohai cloud detection'
+
 source_url 'https://github.com/chef-cookbooks/aws' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/aws/issues' if respond_to?(:issues_url)
 depends 'ohai'
+
+%w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd windows).each do |os|
+  supports os
+end
