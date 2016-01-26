c34b31b9057f4545fde984026761a03040223493
Olivier Bazoud <olivier.bazoud@gmail.com>
2016-01-26 14:57:14 +0100

    Fix elb register/deregister (version 2.7.2) - see http://docs.aws.amazon.com/sdkforruby/api/Aws/ElasticLoadBalancing/Types/Instance.html

--- providers/elastic_lb.rb
+++ providers/elastic_lb.rb
@@ -3,7 +3,7 @@ include Opscode::Aws::Elb
 action :register do
   converge_by("add the node #{new_resource.name} to ELB") do
     target_lb = elb.describe_load_balancers[:load_balancer_descriptions].find { |lb| lb[:load_balancer_name] == new_resource.name }
-    unless target_lb[:instances].detect { |instances| instances.include?(instance_id) }
+    unless target_lb[:instances].detect { |instance| instance.instance_id == instance_id }
       Chef::Log.info("Adding node to ELB #{new_resource.name}")
       elb.register_instances_with_load_balancer(load_balancer_name: new_resource.name, instances: [{ instance_id: instance_id }])
     else
@@ -15,7 +15,7 @@ end
 action :deregister do
   converge_by("remove the node #{new_resource.name} from ELB") do
     target_lb = elb.describe_load_balancers[:load_balancer_descriptions].find { |lb| lb[:load_balancer_name] == new_resource.name }
-    if target_lb[:instances].detect { |instances| instances.include?(instance_id) }
+    if target_lb[:instances].detect { |instance| instance.instance_id == instance_id }
       Chef::Log.info("Removing node from ELB #{new_resource.name}")
       elb.deregister_instances_from_load_balancer(load_balancer_name: new_resource.name, instances: [{ instance_id: instance_id }])
     else
