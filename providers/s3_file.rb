1d13e5035e445ca4a36bf0804085d1db9b76ff0b
Tim Smith <tsmith84@gmail.com>
2016-03-11 11:53:16 -0800

    Remove support for Chef < 11.6.0
    11.6.0 shipped in July of 2013.  We should reduce the complexity by
    removing the check/warning for this old version of Chef.

--- providers/s3_file.rb
+++ providers/s3_file.rb
@@ -23,11 +23,6 @@ action :touch do
 end
 
 def do_s3_file(resource_action)
-  version = Chef::Version.new(Chef::VERSION[/^(\d+\.\d+\.\d+)/, 1])
-  if version.major < 11 || (version.major == 11 && version.minor < 6)
-    Chef::Log.warn('In order to automatically use etag support to prevent re-downloading files from s3, you must upgrade to at least chef 11.6.0')
-  end
-
   remote_path = new_resource.remote_path
   remote_path.sub!(%r{^/*}, '')
 
@@ -42,19 +37,16 @@ def do_s3_file(resource_action)
     mode new_resource.mode
     checksum new_resource.checksum
     backup new_resource.backup
+    headers new_resource.headers
+    use_etag new_resource.use_etag
+    use_last_modified new_resource.use_last_modified
+    atomic_update new_resource.atomic_update
+    force_unlink new_resource.force_unlink
+    manage_symlink_source new_resource.manage_symlink_source
     if node['platform_family'] == 'windows'
       inherits new_resource.inherits
       rights new_resource.rights
     end
     action resource_action
-
-    if version.major > 11 || (version.major == 11 && version.minor >= 6)
-      headers new_resource.headers
-      use_etag new_resource.use_etag
-      use_last_modified new_resource.use_last_modified
-      atomic_update new_resource.atomic_update
-      force_unlink new_resource.force_unlink
-      manage_symlink_source new_resource.manage_symlink_source
-    end
   end
 end
