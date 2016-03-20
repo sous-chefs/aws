9d12be4099d5e0e84483fef6317a38179afbcffd
Tim Smith <tsmith84@gmail.com>
2016-03-20 10:38:53 -0700

    Disable instance monitoring in the suite as well

--- test/fixtures/cookbooks/aws_test/recipes/instance_monitoring.rb
+++ test/fixtures/cookbooks/aws_test/recipes/instance_monitoring.rb
@@ -5,3 +5,9 @@ aws_instance_monitoring 'enable detailed monitoring' do
   aws_secret_access_key node['aws_test']['access_key']
   action :enable
 end
+
+aws_instance_monitoring 'disable detailed monitoring' do
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+  action :disable
+end
