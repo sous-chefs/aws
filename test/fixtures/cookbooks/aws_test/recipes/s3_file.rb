4db4208d4abbf77b8ea14f33ecc33731e2a45ee8
Tim Smith <tsmith84@gmail.com>
2016-03-22 14:47:51 -0700

    a_file vs. an_file

--- test/fixtures/cookbooks/aws_test/recipes/s3_file.rb
+++ test/fixtures/cookbooks/aws_test/recipes/s3_file.rb
@@ -1,6 +1,6 @@
 include_recipe 'aws::default'
 
-aws_s3_file '/tmp/an_file' do
+aws_s3_file '/tmp/a_file' do
   bucket node['aws_test']['bucket']
   remote_path node['aws_test']['s3key']
   aws_access_key node['aws_test']['key_id']
@@ -8,7 +8,7 @@ aws_s3_file '/tmp/an_file' do
 end
 
 # do it again. this will produce an INFO log message and skip the actual action
-aws_s3_file '/tmp/an_file' do
+aws_s3_file '/tmp/a_file' do
   bucket node['aws_test']['bucket']
   remote_path node['aws_test']['s3key']
   aws_access_key_id node['aws_test']['key_id']
