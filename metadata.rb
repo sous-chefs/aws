fcc57ea83960b0dfda5d7521efd4f9cc07624b02
Tim Smith <tsmith84@gmail.com>
2016-03-22 21:34:42 -0700

    Release 3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Custom resources for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.0'
+version '3.1.0'
 
 recipe 'aws', 'Installs the aws-sdk gem during compile time'
 recipe 'ec2_hints', 'Adds an EC2 hint file for Ohai cloud detection'
