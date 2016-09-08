0c3240e2d8abbbce5a37d4159dafb02e02fa95ec
Tim Smith <tsmith@chef.io>
2016-09-07 17:49:55 -0700

    Warn in the logs if the default recipe is included
    People should be warned that this isn't going to do anything
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,4 @@
 # This cookbook is no longer needed to load the aws-sdk and can be removed
 # from node run_lists without any impact
+
+Chef::Log.warn('The default aws recipe does nothing. See the readme for information on using the aws resources')
