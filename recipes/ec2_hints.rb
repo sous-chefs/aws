f2fda12470ee366fa742111cdeda545e773d67ef
Tim Smith <tsmith@chef.io>
2016-11-25 20:16:47 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/ec2_hints.rb
+++ recipes/ec2_hints.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: aws
+# Cookbook:: aws
 # Recipe:: ec2_hints
 #
-# Copyright 2008-2016, Chef Software, Inc.
+# Copyright:: 2008-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
