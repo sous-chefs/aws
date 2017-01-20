34e0e0ae598aa74041ba37ff3e53afdd49fe489e
Arun Bhagyanath <arun.bhagyanath1@hcentive.com>
2017-01-20 13:00:13 +0000

    Cloudwatch Implementation
    Signed-off-by: Arun Bhagyanath <arunbhagyanath@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Custom resources for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '4.1.3'
+version '4.1.4'
 
 recipe 'aws', 'Installs the aws-sdk gem during compile time'
 recipe 'ec2_hints', 'Adds an EC2 hint file for Ohai cloud detection'
