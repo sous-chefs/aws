b17ac3f295903da29371748933ccf3ebbd41bc15
Tim Smith <tsmith@chef.io>
2017-03-21 15:47:56 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Gemfile
+++ Gemfile
@@ -7,6 +7,5 @@
 source 'https://rubygems.org'
 
 gem 'tomlrb'
-gem 'rake'
 gem 'stove'
 gem 'community_cookbook_releaser'
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'aws'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Provides resources for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.2.2'
