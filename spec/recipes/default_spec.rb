7df034eedc41827a7977439965c651b4f79e7f3e
Tim Smith <tsmith@chef.io>
2017-04-18 21:40:19 -0700

    Resolve deprecation warning in the chefspecs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'aws::ec2_hints' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
 
   it 'creates the ohai hint' do
     expect(chef_run).to create_ohai_hint('ec2').at_compile_time
