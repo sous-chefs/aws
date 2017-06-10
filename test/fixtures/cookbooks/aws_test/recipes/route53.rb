4c675d934cf70b68985282f18a47e21159c6bf2d
Tim Smith <tsmith@chef.io>
2017-06-09 17:11:23 -0700

    Update testing to create a zone and add a record
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/aws_test/recipes/route53.rb
+++ test/fixtures/cookbooks/aws_test/recipes/route53.rb
@@ -1,54 +1,51 @@
-node.default['route53']['zone_id'] = 'Z2DEMODEMODEMO'
-
-node.default['records']['generic_record']['name'] = 'kitchen-test.yourdomain.org'
-node.default['records']['generic_record']['value'] = '16.8.4.3'
-node.default['records']['generic_record']['type'] = 'A'
-node.default['records']['generic_record']['ttl'] = 3600
-
-node.default['records']['alias_record']['name'] = 'kitchen-test-alias.yourdomain.org'
-node.default['records']['alias_record']['alias_target']['dns_name'] = 'dns-name'
-node.default['records']['alias_record']['alias_target']['hosted_zone_id'] = 'host-zone-id'
-node.default['records']['alias_record']['alias_target']['evaluate_target_health'] = false
-node.default['records']['alias_record']['type'] = 'A'
-node.default['records']['alias_record']['run'] = true
-
-aws_route53_zone 'testkitchen.dmz' do
+aws_route53_zone 'Add testkitchen.dmz zone' do
+  name 'testkitchen.dmz'
   description 'A test zone created by Test Kitchen. Delete anytime.'
   aws_access_key node['aws_test']['key_id']
   aws_secret_access_key node['aws_test']['access_key']
 end
 
-aws_route53_record node['records']['generic_record']['name'] do
-  value                 node['records']['generic_record']['value']
-  type                  node['records']['generic_record']['type']
-  ttl                   node['records']['generic_record']['ttl']
-  zone_id               node['route53']['zone_id']
-  aws_access_key        node['aws_test']['key_id']
+aws_route53_record "Add our node's record" do
+  name 'chefnode.testkitchen.dmz'
+  value node['ipaddress']
+  type 'A'
+  ttl 3600
+  zone_name 'testkitchen.dmz.'
+  overwrite true
+  aws_access_key node['aws_test']['key_id']
   aws_secret_access_key node['aws_test']['access_key']
-  overwrite             true
-  action                :create
-  mock                  true
 end
 
-aws_route53_record node['records']['alias_record']['name'] do
-  alias_target          node['records']['alias_record']['alias_target']
-  type                  node['records']['alias_record']['type']
-  zone_id               node['route53']['zone_id']
-  aws_access_key        node['aws_test']['key_id']
+# aws_route53_record "Add our node's alias record" do
+#   name 'chefnode-alias.testkitchen.dmz'
+#   alias_target 'chefnode-alias.testkitchen.dmz'
+#   type 'A'
+#   zone_name 'testkitchen.dmz.'
+#   overwrite true
+#   aws_access_key node['aws_test']['key_id']
+#   aws_secret_access_key node['aws_test']['access_key']
+# end
+
+# aws_route53_record "Delete our node's record" do
+#   name 'chefnode-alias.testkitchen.dmz'
+#   zone_name 'testkitchen.dmz.'
+#   action :delete
+#   aws_access_key node['aws_test']['key_id']
+#   aws_secret_access_key node['aws_test']['access_key']
+# end
+
+aws_route53_record "Delete our node's record" do
+  name 'chefnode.testkitchen.dmz'
+  zone_name 'testkitchen.dmz.'
+  type 'A'
+  action :delete
+  aws_access_key node['aws_test']['key_id']
   aws_secret_access_key node['aws_test']['access_key']
-  overwrite             true
-  action                :create
-  only_if               { node['records']['alias_record']['run'] }
-  mock                  true
 end
 
-aws_route53_record "#{node['records']['generic_record']['name']}_delete" do
-  name                  node['records']['generic_record']['name']
-  value                 node['records']['generic_record']['value']
-  type                  node['records']['generic_record']['type']
-  zone_id               node['route53']['zone_id']
-  aws_access_key        node['aws_test']['key_id']
+aws_route53_zone 'Delete testkitchen.dmz zone' do
+  name 'testkitchen.dmz'
+  aws_access_key node['aws_test']['key_id']
   aws_secret_access_key node['aws_test']['access_key']
-  action                :delete
-  mock                  true
+  action :delete
 end
