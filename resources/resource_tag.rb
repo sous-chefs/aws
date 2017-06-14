f56671544574a42cc5a8ac20e3753d41e6ac70b7
Tim Smith <tsmith@chef.io>
2017-06-13 23:04:30 -0700

    Convert to a custom resource
    Remove current_resource usage since it won't work with new_resource and the ec2 connection initialization
    Convert resource_id to a name property to avoid funky logic that was basically doing the same thing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/resource_tag.rb
+++ resources/resource_tag.rb
@@ -1,20 +1,85 @@
-actions :add, :update, :remove, :force_remove
-default_action :update
-
-state_attrs :aws_access_key,
-            :resource_id,
-            :tags
-
-attribute :resource_id,           kind_of: [String, Array], regex: /(i|snap|vol)-[a-fA-F0-9]{8}/
-attribute :tags,                  kind_of: Hash, required: true
+property :resource_id,           [String, Array], regex: /(i|snap|vol)-[a-fA-F0-9]{8}/, name_property: true
+property :tags,                  Hash, required: true
 
 # authentication
-attribute :aws_access_key,        kind_of: String
-attribute :aws_secret_access_key, kind_of: String
-attribute :aws_session_token,     kind_of: String
-attribute :aws_assume_role_arn,   kind_of: String
-attribute :aws_role_session_name, kind_of: String
-attribute :region,                kind_of: String, default: lazy { fallback_region }
-
+property :aws_access_key,        String
+property :aws_secret_access_key, String
+property :aws_session_token,     String
+property :aws_assume_role_arn,   String
+property :aws_role_session_name, String
+property :region,                String, default: lazy { fallback_region }
 
 include AwsCookbook::Ec2 # needed for aws_region helper
+
+action :update do
+  Chef::Log.debug("The current tags on the node are #{current_tags}")
+
+  updated_tags = current_tags.merge(new_resource.tags)
+  if updated_tags.eql?(current_tags)
+    Chef::Log.debug("AWS: Tags for resource #{new_resource.resource_id} are unchanged")
+  else
+    # tags that begin with "aws" are reserved
+    converge_by("Updating the following tags for resource #{new_resource.resource_id} (skipping AWS tags): " + updated_tags.inspect) do
+      updated_tags.delete_if { |key, _value| key.to_s =~ /^aws/ }
+      ec2.create_tags(resources: [new_resource.resource_id], tags: updated_tags.collect { |k, v| { key: k, value: v } })
+    end
+  end
+end
+
+action :add do
+  Chef::Log.debug("The current tags on the node are #{current_tags}")
+
+  new_resource.tags.each do |k, v|
+    if current_tags.keys.include?(k)
+      Chef::Log.debug("AWS: Resource #{new_resource.resource_id} already has a tag with key '#{k}', will not add tag '#{k}' => '#{v}'")
+    else
+      converge_by("add tag '#{k}' with value '#{v}' on resource #{new_resource.resource_id}") do
+        ec2.create_tags(resources: [new_resource.resource_id], tags: [{ key: k, value: v }])
+      end
+    end
+  end
+end
+
+action :remove do
+  Chef::Log.debug("The current tags on the node are #{current_tags}")
+
+  # iterate over the tags specified for deletion
+  # delete them if they exist on the node and the values match
+  new_resource.tags.keys.each do |key|
+    if current_tags.keys.include?(key) && current_tags[key] == new_resource.tags[key]
+      converge_by("delete tag '#{key}' on resource #{new_resource.resource_id} with value '#{current_tags[key]}'") do
+        ec2.delete_tags(resources: [new_resource.resource_id], tags: [{ key => new_resource.tags[key] }])
+      end
+    else
+      Chef::Log.debug("Key #{key} not present on the node or the specified value (#{new_resource.tags[key]}) does not match the tagged value")
+    end
+  end
+end
+
+action :force_remove do
+  Chef::Log.debug("The current tags on the node are #{current_tags}")
+
+  new_resource.tags.keys do |key|
+    if current_tags.keys.include?(key)
+      converge_by("delete tag '#{key}' on resource #{new_resource.resource_id} with value '#{current_tags[key]}'") do
+        ec2.delete_tags(resources: [new_resource.resource_id], tags: [{ key: key }])
+      end
+    else
+      Chef::Log.debug("Key #{key} not present on the node. Skipping.")
+    end
+  end
+end
+
+action_class do
+  include AwsCookbook::Ec2
+
+  def current_tags
+    @current_tags ||= begin
+      aws_tags = {}
+      ec2.describe_tags(filters: [{ name: 'resource-id', values: [new_resource.resource_id] }])[:tags].map do |tag|
+        aws_tags[tag[:key]] = tag[:value]
+      end
+      aws_tags
+    end
+  end
+end
