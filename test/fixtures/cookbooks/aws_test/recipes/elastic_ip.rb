6871a01eef11d8cbb597c42dffcd4472ca270a80
Tim Smith <tsmith@chef.io>
2017-06-16 14:31:34 -0700

    Update elastic_ip testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/aws_test/recipes/elastic_ip.rb
+++ test/fixtures/cookbooks/aws_test/recipes/elastic_ip.rb
@@ -4,3 +4,10 @@ aws_elastic_ip 'elastic_ip' do
   ip node['aws_test']['elastic_ip']
   action :associate
 end
+
+aws_elastic_ip 'elastic_ip' do
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+  ip node['aws_test']['elastic_ip']
+  action :disassociate
+end
