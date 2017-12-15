18033856c72bf3dd709600bc9e8ee9a84c43826b
Bob DeRosa <chefbob@bascom.com>
2017-12-15 15:27:36 -0500

    Added basic functionality for parameter store
    Signed-off-by: Bob DeRosa <chefbob@bascom.com>

---
+++ resources/ssm_parameter_store.rb
@@ -0,0 +1,124 @@
+property :name,                        String, required: true
+property :description,                 String
+property :value,                       String, required: true
+property :type,                        String, required: true
+property :key_id,              		   String
+property :overwrite,                   [true, false], default: true
+property :with_decryption,             [true, false], default: false
+property :allowed_pattern,             String
+
+# authentication
+property :aws_access_key,        String
+property :aws_secret_access_key, String
+property :aws_session_token,     String
+property :aws_assume_role_arn,   String
+property :aws_role_session_name, String
+property :region,                String, default: lazy { fallback_region }
+
+include AwsCookbook::Ec2 # needed for aws_region helper
+
+# allow use of the property names from the parameter store cookbook
+alias_method :aws_access_key_id, :aws_access_key
+alias_method :aws_region, :region
+
+# Get does not work.  The goal is to be able to call it and return it to a variable.
+action :get do
+    request = {
+      name: name,
+	  with_decryption: with_decryption,
+    }
+    response = ssm_client.get_parameter(request)
+    Chef::Log.debug "Get parameter #{name}"
+end
+
+action :create do
+	if (write_parameter)
+		request = {
+			name: name,
+			description: description,
+			value: value,
+			type: type,
+			key_id: key_id,
+			overwrite: overwrite,
+			allowed_pattern: allowed_pattern,
+			}
+		response = ssm_client.put_parameter(request)
+		Chef::Log.debug "Put parameter #{name}"
+	end
+end
+
+action :delete do
+    request = {
+      name: name,
+	}
+    response = ssm_client.delete_parameter(request)
+    Chef::Log.info "parameter deleted: #{name}"
+end
+
+action_class do
+  include AwsCookbook::Ec2
+
+  def name
+    @name ||= new_resource.name
+  end
+
+  def value
+    @value ||= new_resource.value
+  end
+
+  def type
+    @type ||= new_resource.type
+  end
+
+  def description
+    @description ||= new_resource.description
+  end
+
+  def key_id
+    @key_id ||= new_resource.key_id
+  end
+
+  def overwrite
+    @overwrite ||= new_resource.overwrite
+  end
+
+  def with_decryption
+    @with_decryption ||= new_resource.with_decryption
+  end
+  
+  def allowed_pattern
+    @allowed_pattern ||= new_resource.allowed_pattern
+  end
+
+  def write_parameter
+  # If the paremeter doesn't exist or one of the values has changed and overwrite
+  # is set to true then we'll write the parameter.
+    begin
+		request = {
+			name: name,
+			with_decryption: (type == 'SecureString'),
+		}
+		response = ssm_client.get_parameter(request)	 
+		if ((response.parameter.name == name) && (response.parameter.value == value))
+			return false
+		else if (new_resource.overwrite)
+			return true
+		else
+			return false
+		end
+	end
+	rescue Aws::SSM::Errors::ParameterNotFound => msg
+	# Paremeter doesn't exist
+	   Chef::Log.info "get_parameter exception: #{msg}"
+	   return true
+	end
+  end
+	
+  def ssm_client
+    @ssm ||= begin
+      require 'aws-sdk'
+      Chef::Log.debug('Initializing Aws::SSM::Client')
+      create_aws_interface(::Aws::SSM::Client, region: new_resource.region)
+    end
+  end
+end
---
+++ test/fixtures/cookbooks/aws_test/recipes/ssm_parameter_store.rb
@@ -0,0 +1,61 @@
+aws_ssm_parameter_store "create testkitchen record" do
+  name 'testkitchen'
+  description 'testkitchen'
+  value 'testkitchen'
+  type 'String'
+  action :create
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+end
+
+aws_ssm_parameter_store "create test kitchen record" do
+  name '/testkitchen/ClearTextString'
+  description 'Test Kitchen String Parameter'
+  value 'Clear Text Test Kitchen'
+  type 'String'
+  action :create
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+end
+
+aws_ssm_parameter_store "create encrypted test kitchen record with default key" do
+  name '/testkitchen/EncryptedStringDefaultKey'
+  description 'Test Kitchen Encrypted Parameter - Default'
+  value 'Encrypted Test Kitchen Default'
+  type 'SecureString'
+  action :create
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+end
+
+# need to figure out how to test this since it depends on a keyid
+#aws_ssm_parameter_store "create encrypted test kitchen record" do
+#  name '/testkitchen/EncryptedStringCustomKey'
+#  description 'Test Kitchen Encrypted Parameter - Custom'
+#  value 'Encrypted Test Kitchen Custom'
+#  type 'SecureString'
+#  key_id ''
+#  action :create
+#  aws_access_key node['aws_test']['key_id']
+#  aws_secret_access_key node['aws_test']['access_key']
+#end
+
+# Need to create tests for get
+
+aws_ssm_parameter_store "create test kitchen record" do
+  name '/testkitchen/ClearTextString'
+  description 'NewString'
+  value 'NewString'
+  type 'String'
+  overwrite true
+  action :create
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+end
+
+aws_ssm_parameter_store 'delete testkitchen record' do
+  name 'testkitchen'
+  aws_access_key node['aws_test']['key_id']
+  aws_secret_access_key node['aws_test']['access_key']
+  action :delete
+end
