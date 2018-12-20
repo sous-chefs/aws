7a0ebab18fd524905a757f3644901be5d6518f0d
Eduardo A. Bustamante López <dualbus@gmail.com>
2018-12-19 20:28:20 -0800

    Added a basic chefspec test
    PR https://github.com/chef-cookbooks/aws/pull/371 passed the CI run
    (i.e. https://travis-ci.org/chef-cookbooks/aws/jobs/469385237), but the
    recipe fails during compilation.
    
    This change provides a minimal safety net, future recipe compilation errors
    should break the build.
    
    Signed-off-by: Eduardo A. Bustamante López <dualbus@gmail.com>

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,3 +1,11 @@
 require 'spec_helper'
 
-# specs coming here!
+describe 'aws::default' do
+  # Nothing in this test is platform-specific, so use the latest Ubuntu for
+  # simulated data.
+  platform 'ubuntu'
+
+  context 'does not raise an error' do
+    it { expect { chef_run }.to_not raise_error }
+  end
+end
