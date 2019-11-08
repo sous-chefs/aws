362e949fb2bca57be11e003e8ab617a5d924da31
Ben Abrams <me@benabrams.it>
2019-11-08 12:30:40 -0800

    use the helper `platform_family?` where it's currently safe.
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- resources/s3_file.rb
+++ resources/s3_file.rb
@@ -16,6 +16,9 @@ property :force_unlink, [true, false], default: false
 property :manage_symlink_source, [true, false]
 property :virtual_host, [true, false], default: false
 property :s3_url, String
+# Intentionally not using platform_family?('windows') due to a bug/issue
+# where this method is not abvailable in the context of gating properties
+# TODO: update when this is fixed
 if node['platform_family'] == 'windows'
   property :inherits, [true, false], default: true
   property :rights, Hash
@@ -166,7 +169,7 @@ action_class do
       sensitive new_resource.sensitive
       retries new_resource.retries
       retry_delay new_resource.retry_delay
-      if node['platform_family'] == 'windows'
+      if platform_family?('windows')
         inherits new_resource.inherits
         rights new_resource.rights
       end
