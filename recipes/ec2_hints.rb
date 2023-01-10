c7bd7d4d56e90d62b02cf3c43a00654b3815ab88
Mohammad Arshad Siddiqui <93732499+Arshad9999@users.noreply.github.com>
2023-01-10 18:50:00 +0530

    Update ec2_hints.rb

--- recipes/ec2_hints.rb
+++ recipes/ec2_hints.rb
@@ -1,3 +1,11 @@
 # The ec2_hints recipe will be removed in a future version of this cookbook
 
-Chef::Log.warn('The ec2_hint recipe is no longer necessary as Ohai detects EC2 instances automatically. If your instances are not being automatically detected please file a bug at https://github.com/chef/ohai')
+Chef::Log.warn('The ec2_hint recipe is no longer necessary as Ohai detects EC2 instances automatically. 
+It's worth noting that the ec2_hint recipe is not specifically for Ohai, Ohai uses ec2 plugin for detecting the ec2 instances. 
+Ec2_hint is a Chef cookbook which helps you to use some specific attributes from Ohai's ec2 plugin and use them in your cookbook.
+So if you are still using ec2_hint, you might be doing this for some specific functionalities or tasks.
+
+It's always good practice to check the latest version of the cookbook you are using in order to ensure that, 
+you are taking advantage of the latest features and bug fixes.
+    
+If your instances are not being automatically detected please file a bug at https://github.com/chef/ohai')
