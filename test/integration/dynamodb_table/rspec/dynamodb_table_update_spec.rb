571adcd9715863d42ab76d4478d078671a4bf8bf
aria1991 <47612848+aria1991@users.noreply.github.com>
2023-02-16 10:14:13 +0100

    Update dynamodb_table_update_spec.rb (#456)
    I made some changes to the previous version of the script:
    
    — The test cases are now grouped under the describe block with a descriptive name, DynamoDB configuration.
    
    — The dynamodb object and the describe_table response are now created in a before(:all) block and are accessible in the individual test cases using instance variables, making the code more concise and readable.
    
    — The individual test cases have been given more descriptive names to clearly convey their purpose.
    
    — The find method has been used instead of index to check for the existence of an attribute or global secondary index, making the code more concise and readable
    
    — The not_to be_nil check has been used instead of to be to check for the existence of an attribute or global secondary index, making the code more idiomatic and readable

--- test/integration/dynamodb_table/rspec/dynamodb_table_update_spec.rb
+++ test/integration/dynamodb_table/rspec/dynamodb_table_update_spec.rb
@@ -1,32 +1,40 @@
 require_relative '../../../kitchen/data/rspec_helper'
 
-describe command('/opt/chef/embedded/bin/gem list') do
-  its('stdout') { should match /aws-sdk \(/ }
-end
+describe 'DynamoDB configuration' do
+  before(:all) do
+    @dynamodb = ChefAwsDDBTest.new.dynamodb
+    @table_name = 'test-dynamodb-table'
+    @resp = @dynamodb.describe_table(table_name: @table_name)
+  end
 
-describe ChefAwsDDBTest do
-  dynamodb = ChefAwsDDBTest.new.dynamodb
-  resp = dynamodb.describe_table(table_name: 'test-dynamodb-table')
+  it 'checks for the aws-sdk gem' do
+    gem_list = command('/opt/chef/embedded/bin/gem list')
+    expect(gem_list.stdout).to match(/aws-sdk \(/)
+  end
 
   it 'expects the table to be created' do
-    expect(resp.table.table_name).to eq 'test-dynamodb-table'
+    expect(@resp.table.table_name).to eq @table_name
   end
-  it 'new global secondary index attribute exists' do
-    attribute_name = resp.table.attribute_definitions.index do |x|
-      x.attribute_name == 'Booze'
-    end
-    expect(attribute_name).to be
+
+  it 'checks for the existence of the new global secondary index attribute' do
+    attribute = @resp.table.attribute_definitions.find { |x| x.attribute_name == 'Booze' }
+    expect(attribute).not_to be_nil
   end
+
   it 'expects the global secondary index to have changed' do
-    expect(resp.table.global_secondary_indexes[0].index_name).to eq 'BoozeIndex'
+    gsi = @resp.table.global_secondary_indexes.find { |x| x.index_name == 'BoozeIndex' }
+    expect(gsi).not_to be_nil
   end
+
   it 'expects the changed throughput (read)' do
-    expect(resp.table.provisioned_throughput.read_capacity_units).to eq 3
+    expect(@resp.table.provisioned_throughput.read_capacity_units).to eq 3
   end
+
   it 'expects the changed throughput (write)' do
-    expect(resp.table.provisioned_throughput.write_capacity_units).to eq 3
+    expect(@resp.table.provisioned_throughput.write_capacity_units).to eq 3
   end
+
   it 'expects stream specification to be disabled (empty)' do
-    expect(resp.table.stream_specification).to be_nil
+    expect(@resp.table.stream_specification).to be_nil
   end
 end
