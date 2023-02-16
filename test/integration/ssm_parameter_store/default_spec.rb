084293a5934733e402bb309ad2a4e1e9721d3f20
aria1991 <47612848+aria1991@users.noreply.github.com>
2023-02-17 00:11:51 +0100

    Update default_spec.rb
    I refactored the code and made some changes:
    
    This has more descriptive test cases, and uses subject to make the code more concise. It also groups related tests together using describe.one

--- test/integration/ssm_parameter_store/default_spec.rb
+++ test/integration/ssm_parameter_store/default_spec.rb
@@ -1,17 +1,32 @@
-describe json('/tmp/ssm_parameters.json') do
-  # => Get (:get)
-  its('clear_value') { should eq 'Clear Text Test Kitchen' }
-  its('decrypted_value') { should eq 'Encrypted Test Kitchen Default' }
-  its('path1_value') { should eq 'path1_value' }
-  its('path2_value') { should eq 'path2_value' }
-  its('parm1_value') { should eq 'Clear Text Test Kitchen' }
-  its('parm2_value') { should eq 'testkitchen' }
+describe 'SSM Parameter Store' do
+  subject(:params) { json('/tmp/ssm_parameters.json') }
 
-  # => Get Parameters (:get_parameters)
-  its(%w(parameter_values /testkitchen)) { should eq 'testkitchen' }
-  its(%w(parameter_values /testkitchen/ClearTextString)) { should eq 'Clear Text Test Kitchen' }
+  describe.one do
+    context 'when getting parameter values' do
+      it 'returns the clear text value' do
+        expect(params.clear_value).to eq('Clear Text Test Kitchen')
+      end
 
-  # => Get Parameters by Path - Hash Return (:get_parameters_by_path)
-  its(%w(path_values path1)) { should eq 'path1_value' }
-  its(%w(path_values path2)) { should eq 'path2_value' }
+      it 'returns the decrypted value' do
+        expect(params.decrypted_value).to eq('Encrypted Test Kitchen Default')
+      end
+
+      it 'returns the value of a parameter at a specific path' do
+        expect(params.parameter_values['/testkitchen']).to eq('testkitchen')
+        expect(params.parameter_values['/testkitchen/ClearTextString']).to eq('Clear Text Test Kitchen')
+      end
+    end
+
+    context 'when getting parameter values by path' do
+      it 'returns a hash of values at a specific path' do
+        expect(params.path_values['path1']).to eq('path1_value')
+        expect(params.path_values['path2']).to eq('path2_value')
+      end
+    end
+  end
+
+  it 'returns the value of a specific parameter' do
+    expect(params.parm1_value).to eq('Clear Text Test Kitchen')
+    expect(params.parm2_value).to eq('testkitchen')
+  end
 end
